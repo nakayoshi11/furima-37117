4866c0b2f6eefe5b2fe84de2269875c7ba711103
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2021-12-09 11:22:54 +0900

    ログイン・ログアウト実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,2 @@
 class ItemsController < ApplicationController
-  def new
-    
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+        #  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+        #  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
          
-         validates :name, presence: true
+        #  validates :nickname, presence: true
+        #  validates :last_name, presence: true
+        #  validates :first_name, presence: true
+        #  validates :last_name_kana, presence: true
+        #  validates :first_name_kana, presence: true
 end
