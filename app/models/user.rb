aa753c6a208d66d4b5cbb13eb5049896ffed422b
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2021-12-09 15:29:10 +0900

    新規登録エラー表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        #  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-        #  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
          
-        #  validates :nickname, presence: true
-        #  validates :last_name, presence: true
-        #  validates :first_name, presence: true
-        #  validates :last_name_kana, presence: true
-        #  validates :first_name_kana, presence: true
+          validates :nickname, presence: true
+          validates :last_name, presence: true
+          validates_format_of :last_name, with: /\A[一-龥ぁ-ん]/, message: 'is invalid. Input full-width characters'
+          validates :first_name, presence: true
+          validates_format_of :first_name, with: /\A[一-龥ぁ-ん]/, message: 'is invalid. Input full-width characters'
+          validates :last_name_kana, presence: true
+          validates_format_of :last_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'
+          validates :first_name_kana, presence: true
+          validates_format_of :first_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'
+          validates :birth_date, presence: true
+          
 end
