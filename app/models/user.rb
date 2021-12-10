d5f7640daab7d3535fc1e9e64e47822d3820f0aa
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2021-12-10 16:03:21 +0900

    テストコードcommit

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,4 @@ class User < ApplicationRecord
           validates :first_name_kana, presence: true
           validates_format_of :first_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters'
           validates :birth_date, presence: true
-          
 end
