7f66b072adbda46730378150737f8b6fc46377c3
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2021-12-19 11:51:25 +0900

    商品削除機能修正commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,8 +18,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if current_user.id == @item.user_id
     @item.destroy
     redirect_to root_path
+    end
   end
 
   def create
