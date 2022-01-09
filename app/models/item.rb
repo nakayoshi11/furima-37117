5371b34b7ee900a3cf3683c6a264a78b06bb5d2e
nakayoshi11 <yoshikithi.10etoile@gmail.com>
2022-01-09 17:06:07 +0900

    失敗commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,9 @@ class Item < ApplicationRecord
   belongs_to :product_condition
   belongs_to :user
   has_one :purchase_history
-  has_one_attached :image
+  has_many_attached :images
 
-  validates :image, :product_name,:selling_price, :product_description, presence: true
+  validates :images, :product_name,:selling_price, :product_description, presence: true
 
   validates :burden_of_shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :product_category_id, numericality: { other_than: 1 , message: "can't be blank" }
